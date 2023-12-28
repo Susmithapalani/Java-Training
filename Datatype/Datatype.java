f461c9285ff65dc4757526d6448462953234dce2
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2023-12-28 11:19:29 +0530

    Add files via upload

---
+++ Datatype/Datatype.java
@@ -0,0 +1,24 @@
+class Datatype{
+public static void main (String[]args)
+{
+boolean bool=true;
+char c='t';
+byte b=6;
+short s=6;
+long l=6;
+int i=6;
+float f=10.5f;
+double d =10.5d;
+String str="hello";
+System.out.println(str);
+System.out.println(c);
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(bool);
+System.out.println(f);
+System.out.println(d);
+System.out.println(l);
+
+
+}}
