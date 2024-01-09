bdfaa41067f3fe8164ab24dc79d55f34c52f1333
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2024-01-09 10:12:29 +0530

    Add files via upload

---
+++ Votingif/Votingif.java
@@ -0,0 +1,10 @@
+class Votingif{
+public static void main(String[]args){
+int votingAge = 18;
+int userAge = 22;
+if (userAge == votingAge )
+{System.out.println("Eligible");
+
+}
+else{System.out.println("Not Eligible");
+}}}
