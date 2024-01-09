245a4cc457012accd895d3e598e7393ae1766291
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2024-01-09 11:06:55 +0530

    Add files via upload

---
+++ TrianglePattern/TrianglePattern.java
@@ -0,0 +1,10 @@
+class TrianglePattern{
+public static void main(String[]args){
+int height = 4;
+for (int i = 1;i<= height;i++){
+for (int j = 1;j<= i;j++){
+System.out.print("* ");}
+System.out.println();
+}
+}
+}
