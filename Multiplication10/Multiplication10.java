b903be0a77d71f7a154b8fbc592424ac2b46a9e6
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2024-01-10 11:24:34 +0530

    Add files via upload

---
+++ Multiplication10/Multiplication10.java
@@ -0,0 +1,10 @@
+class Multiplication10{
+public static void main(String[]args){
+int N = 10;
+for(int i = 1;i <=10;i++){
+int result = N * i;
+System.out.println(N + "*" + i + "=" + result);
+}
+
+}
+}
