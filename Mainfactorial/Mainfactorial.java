9e486062cd37f96bd78865582304148b5eb041f4
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2024-01-27 19:40:58 +0530

    Add files via upload

---
+++ Mainfactorial/Mainfactorial.java
@@ -0,0 +1,21 @@
+import java.io.Console;
+public class MainFibonnaci
+{
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        String input = console.readLine("Enter the value of N: ");
+        int n = Integer.parseInt(input);
+
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th number in the Fibonacci series is: " + result);
+    }
+
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
