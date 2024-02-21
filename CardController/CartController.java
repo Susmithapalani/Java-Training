a3cb2821bf462d841cdc74b9f452d5d261fdcade
Susmithapalani <155040041+Susmithapalani@users.noreply.github.com>
2024-02-21 11:43:48 +0530

    Add files via upload

---
+++ CardController/CartController.java
@@ -0,0 +1,6 @@
+package com.dolszewski.shop.controller;
+public class CartController{
+public static void main(String[]args){
+System.out.println("Welcome to Java");
+}
+}
---
+++ CardController/OrderController.java
@@ -0,0 +1,6 @@
+package com.dolszewski.shop.controller;
+public class ProductController{
+public static void main(String[]args){
+System.out.println("Welcome to Java");
+}
+}
